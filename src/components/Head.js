6bd5fcd2bee3240f7a7b1aefea539dd633e6c7e2
kingwinchan <kingwinchan@126.com>
2017-10-22 23:17:12 +1100

    open github source code in new page.

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,7 +5,7 @@ const Head = () => {
     <header className="head-wrap">
       <h1 className="logo">Memo</h1>
       <ul className="head-nav iconfont">
-        <li><a href="https://github.com/kingwinchan/react-memo">&#xe63f;</a></li>
+        <li><a target="_blank" href="https://github.com/kingwinchan/react-memo">&#xe63f;</a></li>
       </ul>
     </header>
   );
