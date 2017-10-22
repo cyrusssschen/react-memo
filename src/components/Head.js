4dc130f430f306e9dba60a66721e5c0f3c4fe253
kingwinchan <kingwinchan@126.com>
2017-10-22 23:14:30 +1100

    modify logo in head.

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Head = () => {
   return (
     <header className="head-wrap">
-      <h1 className="logo">Simple Memo</h1>
+      <h1 className="logo">Memo</h1>
       <ul className="head-nav iconfont">
         <li><a href="https://github.com/kingwinchan/react-memo">&#xe63f;</a></li>
       </ul>
